5cde81457d9e0e0399f6c8e293215e286af38e41
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 05:40:01 +0800

    auto commit

--- yolov5_vehicle_plate_det/gen_imgs_path.py
+++ yolov5_vehicle_plate_det/gen_imgs_path.py
@@ -1,6 +1,12 @@
 import os
-
+from pathlib import Path
 
 def get_imgs_path(data_dir='/home/data/'):
     dirs = os.listdir(data_dir)
-    abs_
+    abs_dirs = [os.path.join(data_dir, i) for i in dirs]
+    for d in abs_dirs:
+        files = os.listdir(d)
+        for f in files:
+            p = Path(f)
+            if p.suffix == ''
+        
