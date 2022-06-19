fa717ee592ae5cf6aa0c9aacb07a042336968066
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 05:50:01 +0800

    auto commit

--- yolov5_vehicle_plate_det/gen_imgs_path.py
+++ yolov5_vehicle_plate_det/gen_imgs_path.py
@@ -5,18 +5,25 @@ def get_imgs_path(data_dir='/home/data/'):
     dirs = os.listdir(data_dir)
     abs_dirs = [os.path.join(data_dir, i) for i in dirs]
     det_dirs = []
-    ocr_dirs
+    ocr_dirs = []
     for d in abs_dirs:
         files = os.listdir(d)
         
+        ocr_flag = True
         for f in files:
             p = Path(f)
-            ocr_flag = False
             if p.suffix == '.xml':
-                det_files.append(files)
-                ocr_flag = True
+                det_files.append(d)
+                ocr_flag = False
                 break
-            if ocr_flag:
+        if ocr_flag:
+            ocr_dirs.append[d]
+    
+    for d in det_dirs:
+        
+    
+return det_dirs, ocr_dirs
+            
                 
             
                 
