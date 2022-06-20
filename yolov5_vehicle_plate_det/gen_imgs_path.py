2a54bf3ca81acc0dcc36f0ec12fc8f4c8a7996d3
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 10:30:13 +0800

    auto commit

--- yolov5_vehicle_plate_det/gen_imgs_path.py
+++ yolov5_vehicle_plate_det/gen_imgs_path.py
@@ -21,6 +21,7 @@ def get_imgs_path(data_dir='/home/data/', save_dir_path='/home/data/labels/'):
         if ocr_flag:
             ocr_dirs.append[d]
     
+    # 获得data目录下哪些是
     os.makedirs(save_dir_path, exist_ok='True')
     for d in det_dirs:
         for image_path in glob.glob(os.path.join(d + "*.jpg")):
@@ -32,11 +33,29 @@ def get_imgs_path(data_dir='/home/data/', save_dir_path='/home/data/labels/'):
             with open(save_dir_path + 'all_ocr.txt', 'w') as f1:
                 for train_pwd in train_abs_img_paths:
                     f1.write(train_pwd + '\n')
-                    
-    with open(save_dir_path + 'all_det.txt') as f1:
+    
+    # 生成训练测试集，写入txt文件中
+    with open(os.path.join(save_dir_path + 'all_det.txt')) as f1:
         all_det = f1.readlines()
         random.shuffle(all_det)
         
+        num_imgs = len(all_det)
+        train_percent = 0.9
+        train_abs_img_paths = all_det[:int(train_percent*num_imgs)]
+        test_abs_img_paths = all_det[int(train_percent*num_imgs):]
+    
+        with open(os.path.join(save_dir_path + 'train.txt'), 'w') as f1:
+            for train_pwd in train_abs_img_paths:
+                f1.write(train_pwd)
+
+        with open(os.path.join(save_dir_path + 'test.txt'), 'w') as f1:
+            for test_pwd in test_abs_img_paths:
+                f1.write(test_pwd)
+
+        with open(os.path.join(save_dir_path + 'val.txt'), 'w') as f1:
+            for val_pwd in test_abs_img_paths:
+                f1.write(val_pwd)
+
     
             
 
