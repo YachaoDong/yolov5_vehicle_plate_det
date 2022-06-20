412e3d0895a51163215e01bff60a2e47eaf8dea0
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 14:25:14 +0800

    auto commit

--- yolov5_vehicle_plate_det/run.sh
+++ yolov5_vehicle_plate_det/run.sh
@@ -25,6 +25,7 @@ python /project/train/src_repo/yolov5_vehicle_plate_det/xml2labels.py
 # python /project/train/src_repo/yolov5_smoke/split_train_val.py
 
 #执行YOLOV5训练脚本
+# pip install -r /project/train/src_repo/yolov5_vehicle_plate_det/requirements.txt
 python /project/train/src_repo/yolov5_vehicle_plate_det/train.py   --batch-size 32 --weights /project/train/models/yolov5s.pt
 
 python /project/train/src_repo/yolov5_vehicle_plate_det/train.py   --batch-size 16 --weights /project/train/models/yolov5s.pt 
