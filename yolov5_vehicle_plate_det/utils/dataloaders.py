4b923df137948a5be7e40b12d837222d823983ce
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 18:02:47 +0800

    auto commit

--- yolov5_vehicle_plate_det/utils/dataloaders.py
+++ yolov5_vehicle_plate_det/utils/dataloaders.py
@@ -396,8 +396,13 @@ class LoadStreams:
     def __len__(self):
         return len(self.sources)  # 1E12 frames = 32 streams at 30 FPS for 30 years
 
-
+    
 def img2label_paths(img_paths):
+    # Define label paths as a function of image paths
+    
+    return ['/project/train/src_repo/dataset/labels/' + x.strip().split(os.sep)[-1].split('.')[0] + '.txt' for x in img_paths]    
+
+def ori_label_paths(img_paths):
     # Define label paths as a function of image paths
     sa, sb = f'{os.sep}images{os.sep}', f'{os.sep}labels{os.sep}'  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
