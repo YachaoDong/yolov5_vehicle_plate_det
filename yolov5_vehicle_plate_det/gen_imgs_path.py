be0d5477e59ce97add15bd5f1ddebbe4faf9dfbc
cvmart-dev-4467-tencent2 <cvmart-dev-4467-tencent2@cvmart.net>
2022-06-20 17:40:07 +0800

    auto commit

--- yolov5_vehicle_plate_det/gen_imgs_path.py
+++ yolov5_vehicle_plate_det/gen_imgs_path.py
@@ -10,50 +10,11 @@ import argparse
 import glob
 import shutil
 
-def gen_imgs_path(data_dir='/home/data/', save_dir_path='/home/data/vehicle_data/', save_img_path='/home/data/vehicle_data/images/'):
-    dirs = os.listdir(data_dir)
-    abs_dirs = [os.path.join(data_dir, i) for i in dirs]
-    det_dirs = []
-    ocr_dirs = []
-    
-    # 获得data目录下哪些是检测数据集的目录，哪些是ocr数据集目录
-    for d in abs_dirs:
-        files = os.listdir(d)
-        
-        ocr_flag = True
-        for f in files:
-            p = Path(f)
-            if p.suffix == '.xml':
-                det_dirs.append(d)
-                ocr_flag = False
-                break
-        if ocr_flag:
-            ocr_dirs.append(d)
-    
-    # 将det 和 ocr 的 img 绝对路径写入txt中
-    # os.makedirs(save_dir_path, exist_ok='True')
-    
-    for d in det_dirs:
-        with open(save_dir_path + 'all_det.txt', 'w') as f1:
-            for image_path in glob.glob(os.path.join(d, "*.jpg")):
-                image_name = image_path.split(os.sep)[-1].split('.')[0]
-                f1.write(join(save_img_path, (image_name + '.jpg')) + '\n')
-
-        with open(save_dir_path + 'all_det_xmls.txt', 'a') as f2:
-            for image_path in glob.glob(os.path.join(d, "*.jpg")):
-                f2.write(image_path.replace('.jpg', '.xml') + '\n')
-        
-        
-                    
-    
-    for d in ocr_dirs:
-        for image_path in glob.glob(os.path.join(d, "*.jpg")):
-            with open(save_dir_path + 'all_ocr.txt', 'a') as f1:
-                f1.write(image_path + '\n')
+def gen_imgs_path():
     
     
     # 生成训练测试集，写入txt文件中
-    with open(os.path.join(save_dir_path, 'all_det.txt')) as f1:
+    with open('/home/data/vehicle_data/all_det_imgs.txt', 'r') as f1:
         all_det = f1.readlines()
         random.shuffle(all_det)
         
@@ -62,15 +23,15 @@ def gen_imgs_path(data_dir='/home/data/', save_dir_path='/home/data/vehicle_data
         train_abs_img_paths = all_det[:int(train_percent*num_imgs)]
         test_abs_img_paths = all_det[int(train_percent*num_imgs):]
     
-        with open(os.path.join(save_dir_path, 'train.txt'), 'w') as f1:
+        with open('/home/data/vehicle_data/train.txt', 'w') as f1:
             for train_pwd in train_abs_img_paths:
                 f1.write(train_pwd)
 
-        with open(os.path.join(save_dir_path, 'test.txt'), 'w') as f1:
+        with open('/home/data/vehicle_data/test.txt', 'w') as f1:
             for test_pwd in test_abs_img_paths:
                 f1.write(test_pwd)
 
-        with open(os.path.join(save_dir_path, 'val.txt'), 'w') as f1:
+        with open('/home/data/vehicle_data/val.txt', 'w') as f1:
             for val_pwd in test_abs_img_paths:
                 f1.write(val_pwd)
     
